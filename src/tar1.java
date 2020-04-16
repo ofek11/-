830a5e813aa15fc9e0376b005345c06241488a23
ofek11 <54708885+ofek11@users.noreply.github.com>
2020-04-17 02:41:51 +0300

    Update tar1.java

--- src/tar1.java
+++ src/tar1.java
@@ -1,39 +1,51 @@
-import java.util.Arrays;
 public class tar1 {
-	public static int[] tar_1 (int[] a, int[] b)
+	public static  int[] tar_1 (int[] a, int[] b)
 	{
-		int lenA = a.length-1;
-	    int lenB = b.length-1;
-	    int contA = 0;
-	    int contB = 0;
-	    int i = 0;
-	    int[] OrdList = new int[(lenA+lenB+2)];
-	    while (contA + contB <= lenA+lenB)
-	    {
-	    	if (a[contA] < b[contB])
-	    	{
-	    		OrdList[i] = a[contA];
-	    		i++;
-	    		if(contA<lenA) {
-		    		contA++;	
-	    		}
-	    	}
-	    	else
-	    	{
-	    		OrdList[i] = b[contB];
-	    		i++;
-	    		if(contB<lenB) {
-		    		contB++;	
-	    		}
-	    	}
-	    }
-		    return OrdList;
-		  }
-		  public static void main(String[] args) {
-		        
-		    int[] c =  new int[] {1,2,4,6,8};
-		    int[] f =   new int[] {1,3,6,9,11,15};
-		    int[] g = tar_1(c,f);
-		    System.out.print(Arrays.toString(g));
-		  }
-		}
+		boolean check = true;
+		int numA=0;
+		int numB=0;
+		int p = 0;
+		int[] ordlist = new int[a.length+b.length];
+		for (int len = 0; len<ordlist.length-1;len++)
+		{
+			if(check)
+			{
+				if(numB==a.length-1)
+				{
+					check =false;
+					numB = a.length-1;
+				}
+				for (int i = numA; i < a.length;i++)
+				{
+					if(a[i]<=b[numB])
+					{		
+						ordlist[p] = a[i];
+						p++;
+						check = false;
+						numA++;
+					}
+					if(!check)
+					{
+						if(numA==b.length-1)
+						{
+							check =false;
+							numA = b.length-1 ;
+						}				
+						for (int g = numB; g < b.length;g++)
+						{
+							if(b[g]<a[numA])
+							{							
+								ordlist[p] = b[g];
+								p++;
+								check = true;
+								numB++;
+		}	}	}	}	}	}
+		return ordlist;
+	}
+	public static void main(String[] args)	  
+	{
+		int[] c =  {1,2,4,6,8};
+		int[] f =   {3,5,9,11,15};
+		tar_1(c,f);
+	}
+}
